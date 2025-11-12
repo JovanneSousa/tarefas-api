395967ed3c80e9ba1bd2643c77d0e95176aa8408
Jovane Maciel De Sousa <jovane.sousa@redeflex.com.br>
2025-11-12 16:36:11 -0400

    adicionando environment variables

--- Program.cs
+++ Program.cs
@@ -5,9 +5,12 @@ using tarefas_api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION");
+var webapp = Environment.GetEnvironmentVariable("MEU_APP");
+
 // Add services to the container.
 builder.Services.AddDbContext<TarefaContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString(connectionString)));
 builder.Services.AddScoped<ITarefaRepository, TarefaRepository>();
 builder.Services.AddScoped<ITarefaService, TarefaService>();
 
@@ -17,8 +20,10 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("PermitirMeuSite", policy =>
     {
-        policy.AllowAnyOrigin()
-                      .AllowAnyHeader();
+        policy.WithOrigins(webapp)
+                .AllowCredentials()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
     });
 });
 
